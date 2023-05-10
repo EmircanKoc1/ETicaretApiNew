14edd450d45c00df7ee73014d01bfcc58b48e68b
Emircan <emircandv787@gmail.com>
2023-05-11 02:37:02 +0300

    brandControllerFinished

--- ETicaretApi/Controllers/BrandController.cs
+++ ETicaretApi/Controllers/BrandController.cs
@@ -1,12 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ETicaretApi.Context;
+using ETicaretApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ETicaretApi.Controllers
 {
+
+    [ApiController]
+    [Route("[action]")]
     public class BrandController : Controller
     {
-        public IActionResult Index()
+        ETicaretDbContext context = new();
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetBrandByID(int id)
         {
-            return View();
+            Brand getBrand = await context.Brands.FirstOrDefaultAsync(i => i.BrandID == id);   
+            return Ok(getBrand);
         }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetBrands()
+        {
+            var getBrands = await context.Brands.ToListAsync();
+            return Ok(getBrands);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AddBrand(Brand brand)
+        {
+           await context.Brands.AddAsync(brand);
+           await context.SaveChangesAsync();
+           return Ok(brand);
+
+        }
+
+        [HttpPut("[action]")]
+        public async Task<IActionResult> UpdateBrand(Brand brand)
+        {
+            Brand getBrand = await context.Brands.FirstOrDefaultAsync(i => i.BrandID == brand.BrandID);
+            getBrand = new()
+            {
+                BrandID = brand.BrandID,
+                Name = brand.Name,
+                ImgSrc = brand.ImgSrc
+            };
+            await context.SaveChangesAsync();
+            return Ok(getBrand);
+
+        }
+
+        [HttpDelete("[action]")]
+        public async Task<IActionResult> DeleteByID(int id)
+        {
+            Brand getBrand = await context.Brands.FirstOrDefaultAsync(i => i.BrandID == id);
+            context.Remove(getBrand);
+            await context.SaveChangesAsync();
+            return Ok(getBrand);
+        }
+
+
+
     }
 }
