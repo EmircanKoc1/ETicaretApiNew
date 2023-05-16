5e313ad25dc62cba6f662fb547f47441efc3f93f
Emircan <emircandv787@gmail.com>
2023-05-16 06:39:42 +0300

    order and orderdetails entity add

---
+++ ETicaretApi/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace ETicaretApi.Entities
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+
+        public int UserID { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public float TotalPrice { get; set; }
+
+        public bool OrderStatus { get; set; }
+
+
+
+    }
+}
---
+++ ETicaretApi/Entities/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿namespace ETicaretApi.Entities
+{
+    public class OrderDetail
+    {
+        public int OrderDetailID { get; set; }
+
+        public int OrderID { get; set; }
+
+        public int ProductID { get; set; }
+
+        public int Quantity { get; set; }
+
+        public float UnitPrice { get; set; }
+
+
+
+    }
+}
